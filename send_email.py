f051a0da8b844e5f39aaa03eb04861d44b615ed1
JakeSchroder <jake.schroder@gmail.com>
2023-01-25 12:10:14 -0600

    Add ability to send email

--- send_email.py
+++ send_email.py
@@ -1,11 +1,13 @@
 import yagmail
+import configparser
 
 def send_message(firstName, lastName, email, message):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
     receiver_email = "jake.schroder@gmail.com"
-    sender_email = "bot@gmail.com"
     body = "Name: %s %s \nEmail: %s\nMessage:\n%s" %(firstName, lastName, email, message)
-    yag = yagmail.SMTP(sender_email)
-    
+    yag = yagmail.SMTP(config['EmailLogin']['sender_email'], config['EmailLogin']['sender_password'])
+
     yag.send(
         to=receiver_email,
         subject="Someone wants to linkup!",
